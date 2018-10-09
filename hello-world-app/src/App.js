035be558933c897c85855ce0c45d6f75694b35d0
David Cámara Iglesias <david.c.iglesias@gmail.com>
2018-10-09 22:20:42 +0200

    Slightly Random Now

--- hello-world-app/src/App.js
+++ hello-world-app/src/App.js
@@ -9,14 +9,26 @@ class App extends Component {
     console.log('aw');
   }
 
+  getRandomOption() {
+    const listOptions = [
+      'Mommy, feed me!!!!',
+      'Woooooo!! :D',
+      'I am a wonderful caaaaat!',
+      'Yay!!!',
+      'Cats are amazing, but I\'m even more so!'
+    ];
+    return listOptions[Math.floor(Math.random() * (listOptions.length - 1)) + 1];
+  }
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
         <div className="container">
             <img src={winter} className="App-logo" alt="logo" />
             <p className="App-text">
-              <b>Winter</b><br/>I'm the sweetiest sweetie! :>
+              <b>Winter</b><br/>{this.getRandomOption()}
             </p>
           </div>
         </header>
